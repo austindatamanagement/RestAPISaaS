def52351c89be78630e58fc6950c53029241c517
MEGHA GAJBHIYE <megha@MEGHAs-MacBook-Pro.local>
2019-02-21 15:11:33 -0600

    Average added

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import cx_Oracle
 from flask import Flask
 import json
+import numpy as np
 
 app = Flask(__name__)
 
@@ -32,6 +33,48 @@ def vesseltakedown():
     cur.close()
     connection.close()
     return list_json
+
+@app.route('/video')
+def video():
+    connection = cx_Oracle.connect("admin/DemoATP12345678@demoatp_high")
+    cur = connection.cursor()
+    cur.execute("""
+    declare
+    begin
+    dbms_cloud.put_object( 
+    'OBJ_STORE_CRED', 
+    'https://swiftobjectstorage.us-phoenix-1.oraclecloud.com/v1/gse00014475/VideoBucket/Neurological_Surgery_.mp4'
+    );
+    end;
+    """)
+
+@app.route('/average')
+def average():
+    connection = cx_Oracle.connect("admin/DemoATP12345678@demoatp_high")
+    cur = connection.cursor()
+    cur.execute("select * from benchmark")
+    list_benchmark = []
+    bleeding = []
+    efficiency =[]
+    complexity = []
+    for result in cur:
+        print (type(result))
+        list_benchmark.append(result)
+        bleeding.append(result[1])
+        efficiency.append(result[2])
+        complexity.append(result[3])
+        print (result)
+    avg_bleeding = sum(bleeding)/ float(len(bleeding))
+    avg_efficiency = sum(efficiency)/ float(len(efficiency))
+    avg_complexity = sum(complexity)/ float(len(complexity))
+    # list_json = json.dumps(list_benchmark)
+    average = [avg_bleeding, avg_efficiency, avg_complexity]
+    print (average)
+    list_avg = json.dumps(average)
+    print (list_avg)
+    cur.close()
+    connection.close()
+    return list_avg
 
 @app.route('/')
 @app.route('/index')
